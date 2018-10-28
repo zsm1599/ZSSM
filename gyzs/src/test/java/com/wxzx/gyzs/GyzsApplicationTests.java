d41bde1a0704237f85e2582bcc94380e00a52446
zsm1599 <44487835+zsm1599@users.noreply.github.com>
2018-10-28 20:13:15 +0800

    first commit

---
+++ gyzs/src/test/java/com/wxzx/gyzs/GyzsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wxzx.gyzs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GyzsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
